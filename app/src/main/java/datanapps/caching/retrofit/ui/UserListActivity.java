219fe7899314381d8f19c770f23b99fa55afcc62
yogendra <datanapps@gmail.com>
2019-05-24 15:41:29 +0800

    added pull to refresh

--- app/src/main/java/datanapps/caching/retrofit/ui/UserListActivity.java
+++ app/src/main/java/datanapps/caching/retrofit/ui/UserListActivity.java
@@ -1,6 +1,8 @@
 package datanapps.caching.retrofit.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,6 +15,7 @@ import datanapps.caching.retrofit.R;
 import datanapps.caching.retrofit.network.RetrofitEventListener;
 import datanapps.caching.retrofit.network.users.ApiUserServiceRepoImpl;
 import datanapps.caching.retrofit.network.users.model.BaseUser;
+import datanapps.caching.retrofit.utils.Utils;
 import retrofit2.Call;
 
 public class UserListActivity extends AppCompatActivity {
@@ -20,6 +23,9 @@ public class UserListActivity extends AppCompatActivity {
 
     @BindView(R.id.weather_recycler_view)
     RecyclerView recyclerView;
+
+    @BindView(R.id.pullToRefresh)
+    SwipeRefreshLayout swipeRefreshLayout;
 
 
     private UserListAdapter weatherAdapter;
@@ -33,6 +39,13 @@ public class UserListActivity extends AppCompatActivity {
 
         setRecycleView();
 
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                uploadWeatherData();
+            }
+        });
 
         uploadWeatherData();
     }
@@ -48,19 +61,26 @@ public class UserListActivity extends AppCompatActivity {
 
 
      void uploadWeatherData() {
-        //showProgressDialog("Wait...");
+
+         if (!Utils.isNetworkConnected(this)) {
+             Snackbar.make(recyclerView, "No internet", Snackbar.LENGTH_LONG).show();
+         }
+
+
         ApiUserServiceRepoImpl.getInstance().getWeather(this, new RetrofitEventListener() {
             @Override
             public void onSuccess(Call call, Object response) {
                 if (response instanceof BaseUser) {
                     Log.d("asd", "-----" + ((BaseUser) response).getData().size());
+                    Snackbar.make(recyclerView, "get data", Snackbar.LENGTH_LONG).show();
                     weatherAdapter.setAlbumList(((BaseUser) response).getData());
+                    swipeRefreshLayout.setRefreshing(false);
                 }
             }
 
             @Override
             public void onError(Call call, Throwable t) {
-
+                swipeRefreshLayout.setRefreshing(false);
                 // snack bar that city can not find
             }
         });
